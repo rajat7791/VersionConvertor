c9868c524bbde2d6d7b118490323dc0c3354c7d8
Rajat Jain <rajat_7791@yahoo.com>
2016-09-14 15:29:49 +0530

    VersionToLong method added

--- src/versionconvertor/VersionConvertor.java
+++ src/versionconvertor/VersionConvertor.java
@@ -12,5 +12,26 @@ public class VersionConvertor {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public static long versionToLong(String version) {
+        /**
+         * Version must be '1.2.3' format means 2 dot exactly
+         */
+        long result = 0;
+        String[] versionArr = version.split("\\.");
+        if (versionArr.length != 3) {
+            throw new NumberFormatException("Version should be of format x.y.z");
+        }
+        for (int i = 2; i >= 0; i--) {
+            long versionNum = Long.parseLong(versionArr[2 - i]);
+            //left shifting 16,8,0 and bitwise OR
+            //1. 1 << 16
+            //2. 2   << 8
+            //3. 3   << 0
+            result |= versionNum << (i * 8);
+        }
+        System.out.println("Version: " + version + " | LongValue: " + result);
+        return result;
+    }
+
 }
